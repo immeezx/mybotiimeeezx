5ca347065010a74bf85fa17dde245ca0c912acdf
immeezx <47362425+immeezx@users.noreply.github.com>
2019-02-08 21:33:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,29 +20,33 @@ client.on('message', msg => {
 
 
 
-client.on('message', message => {
-var prefix = "*" // البريفكس
-if(message.content.startsWith(prefix +"info")){ // الامر
-  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`**هذه الخاصية للادارة فقط** ❎ `)
-if(!message.channel.guild) return message.reply(' ');
-const millis = new Date().getTime() - message.guild.createdAt.getTime();
-const now = new Date();
-dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-var embed  = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-.addField("**🆔 Server ID:**", message.guild.id,true)
-.addField("**📅 Created On**", message.guild.createdAt.toLocaleString(),true)
-.addField("**👑 Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
-.addField("**👥 Members**",`[${message.guild.memberCount}]`,true)
-.addField('**💬 Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
-.addField("**🌍 Others **" , message.guild.region,true)
-.addField("**🔐 Roles **",`**[${message.guild.roles.size}]** Role `,true)
-.setColor('#000000')
-message.channel.sendEmbed(embed)
- 
+client.on('message', msg => {
+var prefix = "*";
+  if(!msg.guild) return;
+    if (msg.content.startsWith(prefix +'cc')) {
+     let args = msg.content.split(" ").slice(1);
+    if(!msg.channel.guild) return msg.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+let ra3d = new Discord.RichEmbed()
+.setThumbnail(msg.author.avatarURL)
+.setDescription(`ان كنت تريد انشاء شات كتابي اظغط علي الايموجي✏`)                                                                                                                                                                       
+.setFooter('لديك دقيقه للاختيار')
+msg.channel.send(ra3d).then(message => {
+    
+    
+ message.react('✏').then(r=>{
+                              
+    
+ let Text = (reaction, user) => reaction.emoji.name === '✏' && user.id === msg.author.id;
+
+ let tt  = message.createReactionCollector(Text, { time: 60000 });
+
+tt.on("collect", r => {
+msg.guild.createChannel(args.join(' '), 'text');
+    msg.channel.send(`☑ تم انشاء شات كتابي بنجاح : \`${args}\``)
+    msg.delete();
+})
+})
+})
 }
 });
 
