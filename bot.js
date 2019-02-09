da3e63fd4335a4dd0ce436759c99e09f0a3cc486
immeezx <47362425+immeezx@users.noreply.github.com>
2019-02-09 14:27:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,45 +86,28 @@ msg.guild.createChannel(args.join(' '), 'voice');
 
 
 
-Majd.on('message', message => { //clear
-    if(!message.channel.guild) return;
- if(message.content.startsWith(prefix + 'clear')) {
- if(!message.channel.guild) return message.channel.send('**هذا الامر فقط للسيرفرات**').then(m => m.delete(5000));
- if(!message.member.hasPermission('MANAGE_MESSAGE')) return      message.channel.send('**ليس لديك برمشن manage message`' );
- let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
- let request = `Requested By ${message.author.username}`;
- message.channel.send(`**هل انت متأكد من حذف الشات؟**`).then(msg => {
- msg.react('✅')
- .then(() => msg.react('❌'))
- .then(() =>msg.react('✅'))
+client.on('message', message => {
+    if (message.content === "*createroles") {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_ROLES`` **Premission**`);
 
- let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
- let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+                     message.guild.createRole({ name: "Owner", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Co-Owner", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Leader", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Co-Leader", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "King", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Qween", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "HighNiss", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Pros", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "VIP+", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "VIP", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Actve", color: "#ffffff", permissions: [] })
+                     message.guild.createRole({ name: "Members", color: "#ffffff", permissions: [] })
+        
 
- let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
- let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
- reaction1.on("collect", r => {
- message.channel.send(`سينحذف الشات ...`).then(m => m.delete(5000));
- var msg;
-         msg = parseInt();
-
-       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-       message.channel.sendMessage("", {embed: {
-         title: "`` تــــم حذف الشات ``",
-         color: 0x06DF00,
-         footer: {
-
-         }
-       }}).then(msg => {msg.delete(3000)});
-
- })
- reaction2.on("collect", r => {
- message.channel.send(`**تم الغاء حذف الشات**`).then(m => m.delete(5000));
- msg.delete();
- })
- })
- }
- }); 
+message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم صناعه الرتب **')
+}
+});
  
 
 
